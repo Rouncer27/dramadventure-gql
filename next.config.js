6da2c330e9150f8c1a38e711adb52a8245e1d41d
Rouncer27 <trevor@switchbackcreative.ca>
2023-10-02 12:21:07 -0600

    added fixes

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const withFonts = require("next-fonts");
 
 const nextConfig = withFonts({
   reactStrictMode: true,
+  compiler: {
+    styledComponents: true,
+  },
 });
 
 module.exports = nextConfig;
